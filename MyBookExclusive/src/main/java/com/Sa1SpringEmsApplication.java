076238151ade9a26c8350876c51d2e32222a8045
Kwazikonke Mbatha <49206158+kwazikonke@users.noreply.github.com>
2024-01-28 23:43:17 +0200

    First Create

---
+++ MyBookExclusive/src/main/java/com/Sa1SpringEmsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa1SpringEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa1SpringEmsApplication.class, args);
+	}
+
+}
